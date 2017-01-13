7cc5f30c2c82093732525fa32a2194c21dc05256
Fabricio Nogueira <nogsantos@gmail.com>
2017-01-13 16:43:14 -0200

    :fire: Enable to pass an array list to tests

--- core/mocha/test_runner.js
+++ core/mocha/test_runner.js
@@ -10,23 +10,32 @@ class TestRunner {
      * 
      * @param String dir The directory to files to test
      * @param Include router Load the app routers
+     * @param Array ordered_files_to_test An ordered list to make the test
      */
-    constructor(dir, router) {
+    constructor(dir, router, ordered_files_to_test) {
         let tests = [];
-        let addTest = dir => { // recursive add files to tests
-            return filename => {
-                if (!path.extname(filename) && filename[0] !== '.') {
-                    let nextDir = path.resolve(dir, filename);
-                    return fs.readdirSync(nextDir).forEach(addTest(nextDir));
-                }
-                let Test = require(path.resolve(dir, filename));
+        let addTest = [];
+        if (ordered_files_to_test && ordered_files_to_test.length > 0) {
+            ordered_files_to_test.forEach(file => {
+                let Test = require(path.resolve(dir, `${file}_test.js`));
                 tests.push(new Test(this));
+            }, this);
+        } else {
+            addTest = dir => { // recursive add files to tests
+                return filename => {
+                    if (!path.extname(filename) && filename[0] !== '.') {
+                        let nextDir = path.resolve(dir, filename);
+                        return fs.readdirSync(nextDir).forEach(addTest(nextDir));
+                    }                    
+                    let Test = require(path.resolve(dir, filename));
+                    tests.push(new Test(this));
+                };
             };
-        };
-        let testDir = path.resolve(process.cwd(), dir || '/');
-        fs.readdirSync(testDir).forEach(addTest(testDir));
-        this._tests = tests; // array 
+            let testDir = path.resolve(process.cwd(), dir || '/');
+            fs.readdirSync(testDir).forEach(addTest(testDir));
+        }
         this.router = router;
+        this._tests = tests; // array 
     }
     /**
      * Mocha, do the tests 
